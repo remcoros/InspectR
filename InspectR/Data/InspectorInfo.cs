49a88d7fcd0576d58324ab2bfd6486d14cbcd83a
Remco Ros <r.ros@proscat.nl>
2012-11-09 15:58:07 +0100

    keep datecreated of inspector

--- InspectR/Data/InspectorInfo.cs
+++ InspectR/Data/InspectorInfo.cs
@@ -6,14 +6,18 @@ namespace InspectR.Data
     {
         public Guid Id { get; protected set; }
 
-        public string UniqueKey { get; set; }
+        public string UniqueKey { get; protected set; }
 
         public bool IsPrivate { get; set; }
 
-        public InspectorInfo()
+        public DateTime DateCreated { get; protected set; }
+
+        public InspectorInfo(bool isPrivate = false)
         {
             Id = Guid.NewGuid();
             UniqueKey = Id.ToString().GetHashCode().ToString("x");
+            DateCreated = DateTime.Now;
+            IsPrivate = isPrivate;
         }
     }
 }
