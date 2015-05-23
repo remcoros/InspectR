c6aa606c072e1af2761f72eb9147c77b77c0b6cd
Remco Ros <r.ros@proscat.nl>
2015-05-23 13:58:15 +0200

    fix confirmation code by encoding it

--- InspectR/Controllers/AccountController.cs
+++ InspectR/Controllers/AccountController.cs
@@ -212,6 +212,7 @@
                 // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                 // Send an email with this link
                 string code = await UserManager.GeneratePasswordResetTokenAsync(user.Id);
+                code = HttpUtility.UrlEncode(code);
                 var callbackUrl = Url.Action("ResetPassword", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                 await UserManager.SendEmailAsync(user.Id, "Reset Password", "Please reset your password by clicking <a href=\"" + callbackUrl + "\">here</a>");
                 return RedirectToAction("ForgotPasswordConfirmation", "Account");
